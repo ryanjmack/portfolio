45e4a455a5e2431c8ec5d2aa1c477f80aedc94a1
root <ryanjmack21@gmail.com>
2018-08-15 10:37:36 -0400

    Setup state and pass an event handler to the Navbar component so the Navbar can mutate the App's state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,23 +3,67 @@ import './App.css';
 import Navbar from '../Navbar/Navbar.js';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      display: 'home'
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  // handles a button click from the child component Navbar
+  handleClick(e) {
+    e.preventDefault();
+
+    // each button in the Navbar component has a custom data attribute
+    // 'data-value' -- the state maintained by the App class will
+    // dictate what components are rendered.
+    this.setState({
+      display: e.target.dataset.value
+    });
+  }
+
+
   render() {
-    if (this.props.display === 'about') {
-      // TODO
+    const activeButton = this.state.display;
+
+    // we have 4 main pages to display in our render method
+    if (activeButton === 'about') {
+      return (
+        <div className="App">
+          <Navbar handleClick={this.handleClick} activeButton={activeButton} />
+          <h1>{activeButton}</h1>
+        </div>
+      );
     }
-    else if (this.props.display === 'portfolio') {
-      // TODO
+
+    else if (activeButton === 'portfolio') {
+      return (
+        <div className="App">
+          <Navbar handleClick={this.handleClick} activeButton={activeButton} />
+          <h1>{activeButton}</h1>
+        </div>
+      );
     }
-    else if (this.props.display === 'contact') {
-      // TODO
+
+    else if (activeButton === 'contact') {
+      return (
+        <div className="App">
+          <Navbar handleClick={this.handleClick} activeButton={activeButton} />
+          <h1>{activeButton}</h1>
+        </div>
+      );
     }
+
     else { // home page
       return (
         <div className="App">
-          <Navbar />
-          <h1>{this.props.display}</h1>
+          <Navbar handleClick={this.handleClick} activeButton={activeButton} />
+          <h1>{activeButton}</h1>
         </div>
-      )
+      );
     }
   }
 }

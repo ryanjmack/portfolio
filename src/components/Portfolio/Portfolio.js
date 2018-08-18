b09dcbf5b616082072e0ad093b8dd3991b39313b
Ryan <ryanjmack21@gmail.com>
2018-08-18 16:35:07 -0400

    Update Portfolio component's style

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,38 +5,40 @@ import './Portfolio.css';
 class Portfolio extends React.Component {
 
   generateCards() {
-    const pics = [
-      {fileName: 'calculator', name: 'Calculator', alt: 'calculator', description: 'A calculator built with vanilla JS', src: 'https://github.com/ryanjmack/calculator', demo: 'https://ryanjmack.github.io/calculator/'},
-      {fileName: 'catFacts', name: 'Cat Fact Generator', alt: 'Cat Fact Generator', description: 'Generates random cat facts', src: 'https://github.com/ryanjmack/cat-fact-generator', demo: 'https://ryanjmack.github.io/cat-fact-generator/'},
-      {fileName: 'cycling', name: 'Alp D\'Huez Cycling Visualization', alt: 'Doping in Professional Bicycle Racing', description: 'A D3.js visualization of the fastest climbs up the famous Alp D\'Huez', src: 'https://github.com/ryanjmack/Professional-Cycling-Scatterplot/', demo: 'https://ryanjmack.github.io/Professional-Cycling-Scatterplot/'},
-      {fileName: 'gdp', name: 'US GDP Visualization', alt: 'United States GDP', description: 'A D3.js visualization of the United States GDP from 1947-2018', src: 'https://github.com/ryanjmack/US-GDP-Visualization/', demo: 'https://ryanjmack.github.io/US-GDP-Visualization/'},
-      {fileName: 'heatMap', name: 'Global Land Temperature Visualization', alt: 'Heatmap visualization from 1880-2018', description: 'A D3.js visualization of the mean temperature variance from 1880-2018 compared to the 20th century average', src: 'https://github.com/ryanjmack/Global-Surface-Temperature', demo: 'https://ryanjmack.github.io/Global-Surface-Temperature/'},
-      {fileName: 'landingPage', name: 'Mock Landing Page', alt: 'Mock Landing Page', description: 'A responsive landing page built from scratch', src: 'https://github.com/ryanjmack/mock-landing-page', demo: 'https://ryanjmack.github.io/mock-landing-page'},
-      {fileName: 'markdown', name: 'React Markdown Previewer', alt: 'Markdown Previewer', description: 'A live markdown previewer built with react', src: 'https://github.com/ryanjmack/Markdown-Previewer', demo: 'https://ryanjmack.github.io/Markdown-Previewer'},
-      {fileName: 'mdn', name: 'MDN Documentation Clone', alt: 'Technical Documentation Page', description: 'A clone of a MDN Web Documentation Page', src: 'https://github.com/ryanjmack/fcc-technical-documentation-page', demo: 'https://ryanjmack.github.io/fcc-technical-documentation-page'},
-      {fileName: 'pomodoro', name: 'Pomodoro Clock', alt: 'Pomodoro Clock', description: 'A pomodoro clock built with vanilla JavaScript', src: 'https://github.com/ryanjmack/pomodoro-clock', demo: 'https://ryanjmack.github.io/pomodoro-clock'},
-      {fileName: 'ticTacToe', name: 'Tic Tac Toe', alt: 'Tic Tac Toe', description: 'An unbeatable tic tac toe game, built with vanilla JavaScript', src: 'https://github.com/ryanjmack/tic-tac-toe', demo: 'https://ryanjmack.github.io/tic-tac-toe'},
-      {fileName: 'weather', name: 'Local Weather', alt: 'Local Weather Viewer', description: 'A website that uses geolocation and a weather API to show users their local weather', src: 'https://github.com/ryanjmack/Local-Weather', demo: 'https://ryanjmack.github.io/Local-Weather'}
-    ].map(img => {
-      const src = require(`./images/${img.fileName}.png`);
-
+    const portfolio = [
+      {name: 'Global Temperature Visualization', tags: ['D3.js'], description: 'A visualization of the annual global land temperature variance from 1880-2018', src: 'https://github.com/ryanjmack/Global-Surface-Temperature', demo: 'https://ryanjmack.github.io/Global-Surface-Temperature/'},
+      {name: 'React Markdown Previewer', tags: ['React.js'], description: 'A live markdown previewer', src: 'https://github.com/ryanjmack/Markdown-Previewer', demo: 'https://ryanjmack.github.io/Markdown-Previewer'},
+      {name: 'Unbeatable Tic Tac Toe', tags: ['JavaScript'],  description: 'There are two outcomes: a draw or a loss; minimax is that good', src: 'https://github.com/ryanjmack/tic-tac-toe', demo: 'https://ryanjmack.github.io/tic-tac-toe'},
+      {name: 'Calculator', tags: ['JavaScript'], description: 'A web based calculator', src: 'https://github.com/ryanjmack/calculator', demo: 'https://ryanjmack.github.io/calculator/'},
+      {name: 'Cat Fact Generator', tags: ['Local Storage', 'JavaScript'], description: 'A random cat fact generator', src: 'https://github.com/ryanjmack/cat-fact-generator', demo: 'https://ryanjmack.github.io/cat-fact-generator/'},
+      {name: 'Mock Landing Page', tags: ['JavaScript', 'Responsive Design'], description: 'A responsive landing page', src: 'https://github.com/ryanjmack/mock-landing-page', demo: 'https://ryanjmack.github.io/mock-landing-page'},
+      {name: 'United States GDP Visualization', tags: ['D3.js'], description: 'A visualization of the United States GDP from 1947-2018', src: 'https://github.com/ryanjmack/US-GDP-Visualization/', demo: 'https://ryanjmack.github.io/US-GDP-Visualization/'},
+      {name: 'Alp D\'Huez Cycling Visualization', tags: ['D3.js'], description: 'A visualization of the fastest climbs up the famous Alp D\'Huez', src: 'https://github.com/ryanjmack/Professional-Cycling-Scatterplot/', demo: 'https://ryanjmack.github.io/Professional-Cycling-Scatterplot/'},
+      {name: 'Technical Documentation Page', tags: ['SCSS', 'Responsive Design'], description: 'A clone of a MDN Web Documentation Page', src: 'https://github.com/ryanjmack/fcc-technical-documentation-page', demo: 'https://ryanjmack.github.io/fcc-technical-documentation-page'},
+      {name: 'Pomodoro Clock', tags: ['JavaScript', 'Responsive Design'], description: 'A pomodoro clock with customizable settings', src: 'https://github.com/ryanjmack/pomodoro-clock', demo: 'https://ryanjmack.github.io/pomodoro-clock'},
+      {name: 'Local Weather Viewer', tags: ['Geolocation', 'API'], description: 'Displays user\'s local weather using geolocation', src: 'https://github.com/ryanjmack/Local-Weather', demo: 'https://ryanjmack.github.io/Local-Weather'}
+    ].map(project => {
       return (
         <div className='Portfolio__card'>
-          <figure>
-            <figcaption>{img.name}</figcaption>
-            <img src={src} alt={img.alt}/>
-          </figure>
-          <a href={img.src}>Github</a>
-          <a href={img.demo}>Demo</a>
-          <p>{img.description}</p>
+          <h2>{project.name}</h2>
+          <p>{project.description}</p>
+          <div className='Portfolio__links'>
+            <a href={project.src}>Github</a>
+            <a href={project.demo}>Demo</a>
+          </div>
+          <p>{
+            project.tags.map((tag, i) => {
+              return (<span key={i} className='Portfolio__tag'>{tag}</span>)
+            })
+          }
+          </p>
         </div>
       );
     });
 
-
     return (
       <main className='Portfolio'>
-        {pics}
+        {portfolio}
       </main>
     );
   }
